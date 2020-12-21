04586f1c2100d8fcc0db5db03e979d1315dbc615
Victor Greiveldinger <VGrei@MacBook-Pro-de-Victor-3.local>
2020-12-21 03:31:22 +0100

    Mise au propre du React Modele pour commencer le projet

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const App = () => {
+
+  return (
+    <div className="app">
+      plop
+    </div>
+  );
+};
+
+// == Export
+export default App;
